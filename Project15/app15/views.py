b85d681c18ebf7987f5c4d630ced670fb5a7e0ad
Hari Prem <hariprem.harry@gmail.com>
2021-06-14 18:56:33 +0530

    Commit 14-06

--- Project15/app15/views.py
+++ Project15/app15/views.py
@@ -4,7 +4,21 @@ def showIndex(request):
     if request.method == "POST":
         number1 = request.POST.get("num1")
         number2 = request.POST.get("num2")
-        button = request.POST.get("oper")
-        pass
+        add_button = request.POST.get("cal1")
+        sub_button = request.POST.get("cal2")
+        mul_button = request.POST.get("cal3")
+        div_button = request.POST.get("cal4")
+        if add_button:
+            res = int(number1) + int(number2)
+            return render(request, "index.html", {"message": "add" , "value" : res})
+        elif sub_button:
+            res = int(number1) - int(number2)
+            return render(request, "index.html", {"message": "sub" , "value" : res})
+        elif mul_button:
+            res = int(number1) * int(number2)
+            return render(request, "index.html", {"message": "mul" , "value" : res})
+        else:
+            res = int(number1) / int(number2)
+            return render(request, "index.html", {"message": "div" , "value" : res})
     else:
         return render(request, "index.html")
